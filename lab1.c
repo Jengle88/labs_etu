933c3f4ccba69d19ce6eea96b6e6852e119b94d1
Jengle88 <genya.shkviria@yandex.ru>
2020-10-03 11:53:26 +0300

    add const param and declaration func

--- lab1.c
+++ lab1.c
@@ -1,51 +1,23 @@
 #include <stdio.h>
 
-int max(int array[], int size)
-{
-	int maxElement = array[0];
-	for (int i = 1; i < size; ++i)
-	{
-		if (array[i] > maxElement)
-			maxElement = array[i];
-	}
-	return maxElement;
-}
+int max(int array[], int size);
 
-int min(int array[], int size)
-{
-	int minElement = array[0];
-	for (int i = 1; i < size; ++i)
-	{
-		if (array[i] < minElement)
-			minElement = array[i];
-	}
-	return minElement;
-}
+int min(int array[], int size);
 
+int diff(int array[], int size);
 
-int diff(int array[], int size)
-{
-	return max(array, size) - min(array, size);
-}
+long sum(int array[], int size);
 
-long sum(int array[], int size)
-{
-	int minElement = min(array, size);
-	long sum = 0;
-	for (int i = 0; i < size; ++i)
-	{
-		if (array[i] != minElement)
-			sum += array[i];
-		else
-			return sum;
-	}
-}
+const int MAX_SIZE = 100;
+const int SIGN_PLUS = 1;
+const int SIGN_MINUS = -1;
+
 
 int main()
 {
 	int command = 0;
 	scanf("%d", &command);
-	int array[100];
+	int array[MAX_SIZE];
 	int size = 0;
 	int inputVar = 0;
 	int tempChar = getchar()/*считываем лишний пробел*/;
@@ -54,10 +26,10 @@ int main()
 	{
 		tempChar = getchar();
 		//проверка для отрицательных чисел
-		int sign = 1;//для обработки знака числа
+		int sign = SIGN_PLUS;//для обработки знака числа
 		if (tempChar == '-')
 		{
-			sign = -1;
+			sign = SIGN_MINUS;
 			tempChar = getchar();
 		}
 
@@ -96,3 +68,43 @@ int main()
 	return 0;
 }
 
+
+int max(int array[], int size)
+{
+	int maxElement = array[0];
+	for (int i = 1; i < size; ++i)
+	{
+		if (array[i] > maxElement)
+			maxElement = array[i];
+	}
+	return maxElement;
+}
+
+int min(int array[], int size)
+{
+	int minElement = array[0];
+	for (int i = 1; i < size; ++i)
+	{
+		if (array[i] < minElement)
+			minElement = array[i];
+	}
+	return minElement;
+}
+
+int diff(int array[], int size)
+{
+	return max(array, size) - min(array, size);
+}
+
+long sum(int array[], int size)
+{
+	int minElement = min(array, size);
+	long sum = 0;
+	for (int i = 0; i < size; ++i)
+	{
+		if (array[i] != minElement)
+			sum += array[i];
+		else
+			return sum;
+	}
+}
