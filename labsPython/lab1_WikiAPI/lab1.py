e00a4d6676e1b8e47f7edb2aa28530d2d4d09662
Jengle88 <genya.shkviria@yandex.ru>
2020-10-13 00:30:45 +0300

    add 3 func, have some bugs, simple good

--- labsPython/lab1_WikiAPI/lab1.py
+++ labsPython/lab1_WikiAPI/lab1.py
@@ -15,22 +15,56 @@ def is_right_lang(language):
     return language in wikipedia.languages()
 
 
-# количество слов в строке
-def cnt_word(string):
-    a = list(string.replace('\n', ' ').replace('\t', ' ').split())
-    return len(a)
-
-
 # максимальное количество слов в кратком содержании страницы
 def max_cnt_word_in_summary(array_page):
-    maxCount = -1
+    # количество слов в строке
+    def cnt_word(string):
+        a = list(string.replace('\n', ' ').replace('\t', ' ').split())
+        return len(a)
+
+    maxCount = 0
     answerPage = None
     for page in array_page:
-        cntIterPage = cnt_word(wikipedia.page(page).summary)
-        if maxCount <= cntIterPage:
+        if not is_page_valid(page):  # если не существует
+            continue
+        cntInIterPage = cnt_word(wikipedia.page(page).summary)
+        if maxCount <= cntInIterPage:
             answerPage = page
-            maxCount = cntIterPage
+            maxCount = cntInIterPage
     return [maxCount, answerPage]
+
+
+# ищем цепочку
+def find_chain(pageArray):
+    # ищем промежуточное звено
+    def find_support_chain(pagesSuppArray, index):
+
+        nonlocal pageArray
+
+        for page in pagesSuppArray:
+            if not is_page_valid(page):  # если не существует
+                continue
+            if pageArray[index] in wikipedia.page(page).links:  # если есть ссылка
+                return page
+
+    # поиск первого элемента цепочки и проверка массива
+    firstIndex = 0
+    # двигаем firstIndex, пока идут несущ страницы
+    while firstIndex < len(pageArray) and not is_page_valid(pageArray[firstIndex]):
+        firstIndex += 1
+    if firstIndex == len(pageArray):
+        return []
+
+    # поиск самой цепочки
+    answer = [pageArray[firstIndex]]
+    for i in range(0, len(pageArray) - 1):
+        if not is_page_valid(pageArray[i]):
+            continue
+        linksArray = wikipedia.page(pageArray[i]).links
+        if not (pageArray[i + 1] in linksArray):
+            answer.append(find_support_chain(linksArray, i + 1))
+        answer.append(pageArray[i + 1])
+    return answer
 
 
 # точка входа в программу
@@ -39,7 +73,8 @@ if is_right_lang(array[-1]):
     wikipedia.set_lang(array[-1])
 else:
     print("no results")
-    exit(0)
+    quit(0)
+array.pop()
 print(*max_cnt_word_in_summary(array))
-
-#print(cnt_word(wikipedia.page(array[1]).summary))
+print(find_chain(array))
+# print(cnt_word(wikipedia.page(array[1]).summary))
