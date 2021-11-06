83f6706af4ae411b654a977a19959c4e35b98ce7
Evgeny Shkvirya <zhshkvir@yandex.ru>
2021-11-06 23:16:20 +0300

    cw: add func search and fake insert

--- labsAaSD/cw11_Btree_AVLtree/BTree.h
+++ labsAaSD/cw11_Btree_AVLtree/BTree.h
@@ -2,12 +2,17 @@
 #include <vector>
 #include <cstdlib>
 #include <iostream>
+template<typename T>
+class BTree;
 
 template<typename T>
 class BTreeNode {
-    bool leaf = true; // это лист?
     std::vector<T> keys; // ключи (пользовательский тип)
     std::vector<BTreeNode> children; // массив детей
+public:
+    friend class BTree<T>;
+    BTreeNode(std::vector<T> keys, std::vector<BTreeNode> children) : keys(keys), children(children){};
+    BTreeNode() = default;
 };
 
 template<typename T>
@@ -20,19 +25,57 @@ class BTree {
 public:
     BTree(unsigned int degree, std::vector<T> &array,
           bool (*lessThan)(T a, T b) = [](T a, T b){ return a < b; },
-          bool (*printKey)(T value) = [](T value) {std::cout << value; });
-    void add(T value);
+          void (*printKey)(T value) = [](T value) {std::cout << value; });
+    void insert(T key);
+    void fakeInsert(BTreeNode<T> root);
+    bool contains(T key);
 };
 
 template<typename T>
-BTree<T>::BTree(unsigned int degree, std::vector<T> &array, bool (*lessThan)(T, T), bool (*printKey)(T)) {
+BTree<T>::BTree(unsigned int degree, std::vector<T> &array, bool (*lessThan)(T, T), void (*printKey)(T)) {
     this->degree = degree;
     // TODO: придумать инициализацию дерева (в худшем случае - вставку всего массива) и инициализацию root
     for (int i = 0; i < array.size(); ++i) {
-        add(array[i]); // FIXME исправить
+//        insert(array[i]); // FIXME исправить
     }
     this->lessThan = lessThan;
     this->printKey = printKey;
+}
+
+template<typename T> // Рабочий
+bool BTree<T>::contains(T key) {
+    const BTreeNode<T>* tempNode = &root;
+    while (true) {
+        if (tempNode == nullptr || tempNode->keys.empty()) { // дальше искать нет смысла, выходим
+            return false;
+        }
+        if (lessThan(key, tempNode->keys.front())) { // меньше всех
+            tempNode = tempNode->children.empty() ? nullptr : &(tempNode->children.front()); // безопасная проверка листа
+            continue;
+        }
+        bool rangeFound = false;
+        for (int i = 0; i < tempNode->keys.size(); ++i) {
+            if (key == tempNode->keys[i]) // нашли элемент // TODO заменить на пользовательский equal?
+                return true;
+            else if (lessThan(key, tempNode->keys[i])) { // нашли промежуток для элемента
+                rangeFound = true;
+                tempNode = tempNode->children.empty() ? nullptr : &(tempNode->children[i]);
+                break;
+            }
+        }
+        if (!rangeFound) // больше всех
+            tempNode = tempNode->children.empty() ? nullptr : &(tempNode->children.back());
+    }
+}
+
+template<typename T>
+void BTree<T>::fakeInsert(BTreeNode<T> root) {
+    this->root = root;
+}
+
+template<typename T>
+void BTree<T>::insert(T key) {
+
 }
 
 
--- labsAaSD/cw11_Btree_AVLtree/main.cpp
+++ labsAaSD/cw11_Btree_AVLtree/main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include "BTree.h"
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::vector<int> init(2, 1);
+    BTree<int> bTree(4, init);
+
+
+    /// Search tests
+    BTreeNode<int> root({4, 17, 31}, {
+            BTreeNode<int>({1, 2, 3}, {}),
+            BTreeNode<int>({7, 9, 11, 13, 16}, {}),
+            BTreeNode<int>({19, 26, 27}, {}),
+            BTreeNode<int>({96, 97, 99}, {}),
+    });
+    bTree.fakeInsert(root);
+    std::cout << bTree.contains(5) << "\n";
+    std::cout << bTree.contains(11) << "\n";
+    std::cout << bTree.contains(12) << "\n";
+    std::cout << bTree.contains(13) << "\n";
+    std::cout << bTree.contains(27) << "\n";
+    std::cout << bTree.contains(31) << "\n";
+    std::cout << bTree.contains(100) << "\n";
+    std::cout << bTree.contains(99) << "\n";
+    std::cout << bTree.contains(19) << "\n";
+    std::cout << "-----------------\n";
+    root = BTreeNode<int>({5}, {});
+    bTree.fakeInsert(root);
+    std::cout << bTree.contains(5) << "\n";
+    std::cout << bTree.contains(11) << "\n";
+    std::cout << bTree.contains(12) << "\n";
+    std::cout << bTree.contains(13) << "\n";
+    std::cout << bTree.contains(27) << "\n";
     return 0;
 }
