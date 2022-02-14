698acef5abae2fcd27cf3c1315d7858c13db9dd4
Evgeny Shkvirya <zhshkvir@yandex.ru>
2022-02-15 01:44:55 +0300

    lab1: not optimal, but faster

--- labsPaAA/labs1_backtracking/lab1.kt
+++ labsPaAA/labs1_backtracking/lab1.kt
@@ -1,30 +1,28 @@
 import kotlin.time.ExperimentalTime
 import kotlin.time.measureTime
 
-
-fun fillSquare(field: MutableList<MutableList<Boolean>>, xStart: Int, yStart: Int, size: Int, value: Boolean) {
+fun changeField(field: MutableList<Int>, xStart: Int, size: Int, delta: Int) {
     for (i in xStart until xStart + size) {
-        for (j in yStart until yStart + size) {
-            field[j][i] = value
-        }
+        field[i] += delta
     }
 }
 
-
-fun findFreeCell(field: MutableList<MutableList<Boolean>>, xStart: Int = 0, yStart: Int = 0): Pair<Int, Int> {
-    for (j in yStart until field.size) {
-        for (i in xStart until field[0].size) {
-            if (!field[j][i])
-                return Pair(i, j)
+fun findFreeCell(field: MutableList<Int>): Pair<Int, Int> {
+    var minX = 0
+    for (i in field.indices) {
+        if (field[i] < field[minX]) {
+            minX = i
         }
     }
-    return Pair(-1, -1)
+    if (field[minX] == field.size)
+        return Pair(-1,-1)
+    return Pair(minX, field[minX])
 }
 
 data class Square(val xUpLeft: Int, val yUpLeft: Int, var size: Int)
 
 fun solve(n: Int): List<Square> {
-    val field: MutableList<MutableList<Boolean>> = MutableList(n) { MutableList(n) { false } }
+    val field: MutableList<Int> = MutableList(n) { 0 }
     val squares: MutableList<Square> = mutableListOf()
     var minAnswerSquares: MutableList<Square> = MutableList(n * n) { Square(0, 0, 0) }
     for (i in 0 until n) {
@@ -33,7 +31,7 @@ fun solve(n: Int): List<Square> {
         }
     }
     squares.add(Square(0, 0, n - 1))
-    fillSquare(field, 0, 0, n - 1, true)
+    changeField(field, 0, n-1, n - 1)
     while (squares.isNotEmpty()) {
         val (x, y) = findFreeCell(field)
         if (x == -1 && y == -1) {
@@ -44,37 +42,31 @@ fun solve(n: Int): List<Square> {
                 minAnswerSquares = minAnswerSquares.dropLast(minAnswerSquares.size - squares.size).toMutableList()
             }
             while (squares.isNotEmpty() && (squares.last().size == 1 || squares.size >= minAnswerSquares.size)) {
-                fillSquare(field, squares.last().xUpLeft, squares.last().yUpLeft, squares.last().size, false)
+                changeField(field, squares.last().xUpLeft, squares.last().size, -squares.last().size)
                 squares.removeLast()
             }
             if (squares.isNotEmpty()) {
-                fillSquare(field, squares.last().xUpLeft, squares.last().yUpLeft, squares.last().size, false)
-                fillSquare(field, squares.last().xUpLeft, squares.last().yUpLeft, squares.last().size - 1, true)
-//                for (i in 0 until squares.last().size) {
-//                    field[squares.last().yUpLeft + squares.last().size - 1][squares.last().xUpLeft + i] = false
-//                }
-//                for (i in 0 until squares.last().size) {
-//                    field[squares.last().yUpLeft + i][squares.last().xUpLeft + squares.last().size - 1] = false
-//                }
+                changeField(field, squares.last().xUpLeft, squares.last().size, -squares.last().size)
+                changeField(field, squares.last().xUpLeft, squares.last().size - 1, squares.last().size - 1)
                 squares.last().size--
             }
         } else {
             var delta = 1
-            while (x + delta < field.size && y + delta < field[0].size && !field[y][x + delta] && !field[y + delta][x])
+            while (x + delta < field.size && y + delta < field.size && field[x] + delta < field.size && field[x + delta] <= y)
                 delta++
-            fillSquare(field, x, y, delta, true)
+            changeField(field, x, delta, delta)
             squares.add(Square(x, y, delta))
         }
     }
     return minAnswerSquares
 }
 
-val DEBUG = false
+val DEBUG = true
 
 @OptIn(ExperimentalTime::class)
 fun main() {
     if (DEBUG) {
-        for (n in 2 until 21) {
+        for (n in 2 until 25) {
             println("$n) ${measureTime { solve(n) }}")
         }
     } else {
