ab2a42242242ce78cdf0d0b6d70ec7ecf52f6ed8
Evgeny Shkvirya <zhshkvir@yandex.ru>
2022-09-10 14:36:16 +0300

    lab1: added load and save leaderboard from localStorage

--- labsWEB/lab1/scripts/navScript.js
+++ labsWEB/lab1/scripts/navScript.js
@@ -8,12 +8,20 @@
 // // }
 
 function toGameScreen() {
+    let input = document.getElementById("name_input")
+    if (input && input.value) {
+        localStorage["curr_player_name"] = input.value
+    } else {
+        localStorage["curr_player_name"] = "Player 1"
+    }
     document.getElementById("start_screen").style.display = "none"
     document.getElementById("game_screen").style.display = "flex"
     drawTetrisField()
 }
 
 function toStartScreen() {
+    let playerName = localStorage["curr_player_name"]
+    saveRecordToLeaderboard(playerName);
     document.getElementById("game_screen").style.display = "none"
     document.getElementById("start_screen").style.display = "block"
     clearTetrisField()
